130b6831afe90ef2ff8fed0d2454667d99c8ddef
arshya77 <arshyajabbari@gmail.com>
2019-12-20 04:51:09 -0800

    final

--- param.h
+++ param.h
@@ -11,6 +11,6 @@
 #define LOGSIZE      (MAXOPBLOCKS*3)  // max data blocks in on-disk log
 #define NBUF         (MAXOPBLOCKS*3)  // size of disk block cache
 #define FSSIZE       1000  // size of file system in blocks
-#define QUANTUM      10  // cpu time slots
+#define QUANTUM      5  // cpu time slots
 extern int sflag;          // for cpu scheduling mod
 
